fc8a31d372f0593e81d806a642706094cb5948a9
adnanesaber15 <adnanesaber15@gmail.com>
2020-11-27 05:34:10 +0100

    finished at 5:33 am friday 27 november 2020

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-$(".fade").slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: "linear",
+$(".rec").on("click", function () {
+  let img = $(this).children("img").attr("src");
+  $("body").css("overflow", "hidden");
+  $(".zoomed")
+    .css({ backgroundImage: `url(${img})` })
+    .addClass("open")
+    .on("click", function () {
+      $(".mask").removeClass("open");
+      $(".zoomed").removeClass("open");
+      $("body").css("overflow", "visible");
+    });
+  $(".mask")
+    .addClass("open")
+    .on("click", function () {
+      $(".zoomed").removeClass("open");
+      $(this).removeClass("open");
+      $("body").css("overflow", "visible");
+    });
 });
